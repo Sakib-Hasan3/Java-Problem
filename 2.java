d1084250a2ef7f9ef35940428d3ce5f169fcdd6d
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-09-14 01:28:06 +0600

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,17 @@
+Write a Java program to print the sum of two numbers.
+Test Data:
+74 + 36
+Expected Output :
+110
+  Answer-
+
+//package labproblem2.java;
+
+public class problem2 {
+    public static void main(String[] args)
+    {
+        int x=74,y=36,sum;
+        sum=x+y;
+        System.out.println("summation of two digit :"+sum);
+    }
+}
