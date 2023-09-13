8d633d47faa140452d8dc636f6e5b8ebec5ae848
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-09-14 01:25:33 +0600

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,15 @@
+Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov
+  Answer - 
+  //package labproblem2.java;
+
+public class problem1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("Sakib Hasan");
+    }
+    
+}
