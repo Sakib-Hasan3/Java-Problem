1cb52af2460520ace6090a95d83cffedc0f6ae46
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-10-28 05:00:04 +0600

    Add files via upload

---
+++ JavaProblem/Mathemo.java
@@ -0,0 +1,15 @@
+public class Mathemo {
+    public static void main(String[] args) {
+        int x=20;
+        int y=10;
+
+        int max=Math.max(x,y);
+        System.out.println("Maximum= " +max);
+
+        int min=Math.min(x,y);
+        System.out.println("Minimum= " +min);
+
+
+    }
+    
+}
---
+++ JavaProblem/whatsapp.py
@@ -0,0 +1,11 @@
+import random
+import pyautogui as pg
+import time 
+
+words=('Nice ' , 'Cute')
+time.sleep(8)
+
+for i in range(100)
+a=random.choice(words)
+pg.write("You are a" +a)
+pg.press('enter')
